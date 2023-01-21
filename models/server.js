4187008cb5f433a8ee384f5dbf6d16e14c74356c
Jose Carreño <82679323+ing-jose-rafael@users.noreply.github.com>
2023-01-21 14:13:15 -0500

    Update port

--- models/server.js
+++ models/server.js
@@ -5,7 +5,7 @@ const { dbConnection } = require('../database/configDB');
 class Server{
     constructor(){
         this.app = express();
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
 
         //paths de rutas
         this.paths = {
