329f24b145e0d12fa6910d0dae789c89a0c06e13
josecarreno <ing.jcarreno@gmail.com>
2023-01-25 09:24:26 -0500

    add models to index

--- models/index.js
+++ models/index.js
@@ -1,17 +1,23 @@
 const Asignatura = require('./asignatura');
-const Categoria= require('./categoria');
-const Producto= require('./productos');
+const Categoria = require('./categoria');
+const Curso = require('./curso');
+const Materia = require('./materia');
+const Producto = require('./productos');
 const Profesor = require('./profesor');
 const Role = require('./role');
 const Server = require('./server');
+const UserProf = require('./userProfe');
 const Usuario = require('./usuario');
 
-module.exports={
+module.exports = {
     Asignatura,
     Categoria,
+    Curso,
+    Materia,
     Producto,
     Profesor,
     Role,
     Server,
-    Usuario,
+    UserProf,
+    Usuario
 }
